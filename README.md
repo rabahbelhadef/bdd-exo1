6f77be6de5b88da619f9f1a5e1023600212c8602
rabah <ext-rabah.belhadef@tessi.fr>
2020-10-05 08:30:18 +0200

    exo 1

---
+++ README.md
@@ -0,0 +1,8 @@
+EXO 1 : 
+
+développer un job java permettant de lire le contenu d'un fichier csv et de le sauvegarder en base de donnés dans la table users. 
+
+contraintes : 
+ - les ids devraient être unique. 
+ 
+
---
+++ src/main/java/cnam/bdd/Main.java
@@ -0,0 +1,17 @@
+package cnam.bdd;
+
+import java.io.IOException;
+
+public class Main {
+
+    private static String filePath = "/in/users.csv";
+
+    public static void main(String[] args) throws IOException {
+        //System.out.println(Main.class.getResource("/in/users.csv").getPath());
+        loadFileAddInsertUsers(filePath) ;
+    }
+
+    private static void loadFileAddInsertUsers(String filePath) {
+        //TODO : implement me !
+    }
+}
